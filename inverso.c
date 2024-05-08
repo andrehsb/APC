f29accffc6ca43047aa0043cce8b8831f5c0cd55
andrehsb <168923024+andrehsb@users.noreply.github.com>
2024-05-08 11:15:13 -0300

    Add files via upload

---
+++ inverso.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    int main(){
+        char str[1000], inv[1000];
+        scanf("%s", str);
+        int i;
+        for(i = 0; str[i] != '\0'; i++);
+     
+        for( ; i >= 0; i--){           
+           printf("%c", str[i]);
+        }
+        printf("\n");
+        return 0;
+    }
